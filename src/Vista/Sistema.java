9d8e58e83e0eb4b8e2d909b2a4890f3c3c294d07
fatalbru <fatalbru@gmail.com>
2024-08-28 13:58:53 -0400

    Buscar producto por codigo para agregar a venta

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -10,6 +10,7 @@ import Modelo.Productos;
 import Modelo.ProductosDao;
 import Modelo.Proveedor;
 import Modelo.ProveedorDao;
+import Reportes.Excel;
 import javax.swing.JOptionPane;
 import java.util.List;
 import java.util.ArrayList;
@@ -850,6 +851,11 @@ public class Sistema extends javax.swing.JFrame {
         btnNuevoPro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/nuevo.png"))); // NOI18N
 
         btnExcelPro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/excel.png"))); // NOI18N
+        btnExcelPro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcelProActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
@@ -1318,6 +1324,11 @@ public class Sistema extends javax.swing.JFrame {
         }
         
     }//GEN-LAST:event_btnEditarProActionPerformed
+
+    private void btnExcelProActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcelProActionPerformed
+        // TODO add your handling code here:
+        Excel.reporte();
+    }//GEN-LAST:event_btnExcelProActionPerformed
 
     /**
      * @param args the command line arguments
